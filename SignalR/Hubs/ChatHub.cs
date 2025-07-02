9a94cff074fb46791e945487b309f9bf23c88cae
Ерлан Сакан <ekosha_98@mail.ru>
2025-07-02 22:31:56 +0500

    done

--- SignalR/Hubs/ChatHub.cs
+++ SignalR/Hubs/ChatHub.cs
@@ -8,10 +8,7 @@ public class ChatHub : Hub
 {
     private readonly IMessageService _service;
 
-    public ChatHub(IMessageService service)
-    {
-        _service = service;
-    }
+    public ChatHub(IMessageService service) => _service = service;
 
     public async Task Ping(string user)
     {
@@ -20,32 +17,24 @@ public class ChatHub : Hub
 
     public async Task SendMessage(string user, string text)
     {
-        try
-        {
-            var msg = await _service.AddMessageAsync(user, text, ct);
-            await Clients.All.SendAsync("ReceiveMessage", msg, ct);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-            throw;
-        }
+        var msg = await _service.AddMessageAsync(user, text, Context.ConnectionAborted);
+        await Clients.All.SendAsync("ReceiveMessage", msg);
     }
 
     public async Task<IEnumerable<Message>> GetHistory()
-        => await _service.GetHistoryAsync(ct);
+        => await _service.GetHistoryAsync(Context.ConnectionAborted);
 
     public async Task UpdateMessage(int id, string newText)
     {
-        var msg = await _service.UpdateMessageAsync(id, newText, ct);
+        var msg = await _service.UpdateMessageAsync(id, newText, Context.ConnectionAborted);
         if (msg != null)
-            await Clients.All.SendAsync("MessageUpdated", msg, ct);
+            await Clients.All.SendAsync("MessageUpdated", msg);
     }
 
     public async Task DeleteMessage(int id)
     {
-        var deleted = await _service.DeleteMessageAsync(id, ct);
+        var deleted = await _service.DeleteMessageAsync(id, Context.ConnectionAborted);
         if (deleted)
-            await Clients.All.SendAsync("MessageDeleted", id, ct);
+            await Clients.All.SendAsync("MessageDeleted", id);
     }
 }
